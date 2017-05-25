b7f6c5fd963437e170c98928bc6331f61b3351d0
Grigoriy Melnikov <grigoriy.melnikov@phystech.edu>
2017-05-25 04:10:24 +0300

    kitura

--- Package.swift
+++ Package.swift
@@ -3,5 +3,5 @@ import PackageDescription
 let package = Package(
     name: "SwiftScarrers",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7)
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1)
     ])
